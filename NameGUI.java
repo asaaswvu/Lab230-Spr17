aa7b21aaaf58beb724b912358a223e1fc36a8f9f
Devan MacKenzie Tighe <dmt0001@ubuntu.statler.wvu.edu>
2017-01-17 19:46:25 -0500

    Lab 2

--- NameGUI.java
+++ NameGUI.java
@@ -3,110 +3,121 @@ import java.awt.event.ActionEvent;
 import java.awt.event.*;
 import java.awt.*;
 
-class NameGUI extends JFrame implements ActionListener{
+//Devan Tighe
+//CS230-002
+//January 17, 2017
+
+class NameGUI extends JFrame implements ActionListener
+{
     
-    JTextField txtWord1;
-    JTextField txtWord2;
+	JTextField txtWord1;
+    	JTextField txtWord2;
     
-    NameGUI(){
+   	NameGUI()
+	{
         
-        //create a background panel to put things into, uses BorderLayout & FlowLayout Managers
-        JPanel panelMain = new JPanel(new BorderLayout());
-        JPanel panelTop = new JPanel(new FlowLayout());
-        JPanel panelMiddle = new JPanel(new FlowLayout());
-        JPanel panelBottom = new JPanel(new FlowLayout());
+		//create a background panel to put things into, uses BorderLayout & FlowLayout Managers
+ 	       	JPanel panelMain = new JPanel(new BorderLayout());
+ 	      	JPanel panelTop = new JPanel(new FlowLayout());
+ 	      	JPanel panelMiddle = new JPanel(new FlowLayout());
+    	    	JPanel panelBottom = new JPanel(new FlowLayout());
+  	      
+    	    	//create a button named btnSample with text Sample Button
+    	    	JButton btnName = new JButton("My Name");
+     	   	JButton btnQuit = new JButton("Quit");
+		JButton btnSwap = new JButton("Swap");
         
-        //create a button named btnSample with text Sample Button
-        JButton btnSample = new JButton("Sample Button");
-        JButton btnQuit = new JButton("Quit");
-        JButton btnSwap = new JButton("Swap");
+      	  	//create sample textboxes
+      	  	txtWord1 = new JTextField(15);
+      	  	txtWord2 = new JTextField(15);
         
-        //create sample textboxes
-        txtWord1 = new JTextField(15);
-        txtWord2 = new JTextField(15);
+       	 	//create a label for display
+      	  	JLabel lblSwap = new JLabel("Swap Words!");
         
-        //create a label for display
-        JLabel lblSwap = new JLabel("Swap Words!");
-        
-        //buttons need to say something(ActionCommand) to someone who's listening
-        btnSample.setActionCommand("sample");  //yours will have to be unique
-        btnSample.addActionListener(this);
-        btnQuit.setActionCommand("quit");  //your command will have to be unique
-        btnQuit.addActionListener(this);
-        btnSwap.setActionCommand("swap");  
-        btnSwap.addActionListener(this);
+        	//buttons need to say something(ActionCommand) to someone who's listening
+        	btnName.setActionCommand("name");
+        	btnName.addActionListener(this);
+        	btnQuit.setActionCommand("quit");  //your command will have to be unique
+        	btnQuit.addActionListener(this);
+        	btnSwap.setActionCommand("swap");  
+        	btnSwap.addActionListener(this);
  
-        //Add components to proper panels
-        panelTop.add(btnSample);
-        panelTop.add(btnQuit);
+        	//Add components to proper panels
+        	panelTop.add(btnName);
+        	panelTop.add(btnQuit);
+        	panelBottom.add(lblSwap);
+        	panelBottom.add(btnSwap);
+        	panelBottom.add(txtWord1);
+        	panelBottom.add(txtWord2);
         
-        //panelMiddle.add(btnYourButton);
+        	//Add individual panels to panelMain, applies to BORDERLAYOUT only
+        	//PAGE_START is top of screen
+        	//PAGE_END is bottom of screen
+       		//LINE_START is right side of screen
+        	//LINE_END is left side of screen
+        	//CENTER is center
+        	panelMain.add(panelTop,BorderLayout.PAGE_START);
+        	panelMain.add(panelMiddle,BorderLayout.CENTER);
+        	panelMain.add(panelBottom,BorderLayout.PAGE_END);
+        
+        	//created separate method to generate menu, not necessary, but cleaner
+        	generateMenu();
+        
+        	//set the window size for the app
+        	setSize(800,600);
 
-        panelBottom.add(lblSwap);
-        panelBottom.add(btnSwap);
-        panelBottom.add(txtWord1);
-        panelBottom.add(txtWord2);
-        
-        //Add individual panels to panelMain, applies to BORDERLAYOUT only
-        //PAGE_START is top of screen
-        //PAGE_END is bottom of screen
-        //LINE_START is right side of screen
-        //LINE_END is left side of screen
-        //CENTER is center
-        panelMain.add(panelTop,BorderLayout.PAGE_START);
-        panelMain.add(panelMiddle,BorderLayout.CENTER);
-        panelMain.add(panelBottom,BorderLayout.PAGE_END);
-        
-        //created separate method to generate menu, not necessary, but cleaner
-        generateMenu();
-        
-        //set the window size for the app
-        setSize(800,600);
-
-        //tells java what to do when the class object closes
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setTitle("NameGui Title (how original)");
+        	//tells java what to do when the class object closes
+        	setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        	setTitle("NameGUI - Devan Tighe");
 
         
-        //get visible container and add panelMain to it
-        //EVERYTHING has to be arranged and set before adding to ContentPane
-        getContentPane().add(panelMain);
+        	//get visible container and add panelMain to it
+        	//EVERYTHING has to be arranged and set before adding to ContentPane
+        	getContentPane().add(panelMain);
 
-        //this centers the window in the screen
-        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (int) ((dimension.getWidth() - this.getWidth()) / 2);
-        int y = (int) ((dimension.getHeight() - this.getHeight()) / 2);
-        setLocation(x, y);
+        	//this centers the window in the screen
+        	Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        	int x = (int) ((dimension.getWidth() - this.getWidth()) / 2);
+        	int y = (int) ((dimension.getHeight() - this.getHeight()) / 2);
+        	setLocation(x, y);
         
-        //make sure you can actually see it, starts off false
-        setVisible(true);
-        
-    }
-    private void generateMenu(){
+        	//make sure you can actually see it, starts off false
+        	setVisible(true);
+        	
+   	}
+    	private void generateMenu()
+        {
         //create an empty menu bar
         JMenuBar menuBar = new JMenuBar();
 
         //create a menu  (file, edit, help, etc)
         JMenu menuHelp = new JMenu("Help");
+	JMenu menuFile = new JMenu("File");
         
         //create a menu item and set up its listeners, similar to buttons
         JMenuItem miHelp = new JMenuItem("Help me");
         miHelp.addActionListener(this);
         miHelp.setActionCommand("help");
+
+	JMenuItem miQuit = new JMenuItem("Quit");
+	miQuit.addActionListener(this);
+	miQuit.setActionCommand("quit");
     
         //put together the pieces
         menuHelp.add(miHelp);
+	menuFile.add(miQuit);
         menuBar.add(menuHelp);
+	menuBar.add(menuFile);
 
         //add bar to this JFrame
         setJMenuBar(menuBar);    
-    
-    }
-    public void actionPerformed(ActionEvent evt) {
-        //this method listens to the JFrame's events and performs appropriately
-        switch (evt.getActionCommand()){
-            case "sample":
-                JOptionPane.showMessageDialog(this,"A Sample message dialog box","A plain message",JOptionPane.PLAIN_MESSAGE);
+        }
+    	public void actionPerformed(ActionEvent evt) 
+    	{
+    	    //this method listens to the JFrame's events and performs appropriately
+    	    switch (evt.getActionCommand()){
+            case "name":
+                JOptionPane.showMessageDialog(this,"My Name","Devan Tighe",JOptionPane.PLAIN_MESSAGE);
                 break;
             case "quit" :
                 System.exit(0);
@@ -120,12 +131,12 @@ class NameGUI extends JFrame implements ActionListener{
             case "help" :
                 JOptionPane.showMessageDialog(this,"There is no help for you.","Sorry",JOptionPane.WARNING_MESSAGE);
                 break;
-                
-        }
-    }
+       	    }
+    	}
     
-    public static void main(String [] args){
-        new NameGUI();
-    }
+    	public static void main(String [] args)
+    	{
+    	    new NameGUI();
+    	}
     
 }
