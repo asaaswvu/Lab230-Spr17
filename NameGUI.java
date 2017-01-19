42f4b2e25688406b9546d090897cddecc2f2aaa0
Bradlee Alexander Cruise <bacruise@ubuntu.statler.wvu.edu>
2017-01-19 10:03:30 -0500

    Lab_2

--- NameGUI.java
+++ NameGUI.java
@@ -20,6 +20,7 @@ class NameGUI extends JFrame implements ActionListener{
         JButton btnSample = new JButton("Sample Button");
         JButton btnQuit = new JButton("Quit");
         JButton btnSwap = new JButton("Swap");
+	JButton btnName = new JButton("Name");
         
         //create sample textboxes
         txtWord1 = new JTextField(15);
@@ -35,12 +36,14 @@ class NameGUI extends JFrame implements ActionListener{
         btnQuit.addActionListener(this);
         btnSwap.setActionCommand("swap");  
         btnSwap.addActionListener(this);
- 
+ 	btnName.setActionCommand("name");
+	btnName.addActionListener(this);
         //Add components to proper panels
         panelTop.add(btnSample);
         panelTop.add(btnQuit);
-        
-        //panelMiddle.add(btnYourButton);
+
+        panelMiddle.add(btnName);
+       
 
         panelBottom.add(lblSwap);
         panelBottom.add(btnSwap);
@@ -88,15 +91,24 @@ class NameGUI extends JFrame implements ActionListener{
 
         //create a menu  (file, edit, help, etc)
         JMenu menuHelp = new JMenu("Help");
-        
+        JMenu menuFile = new JMenu("File");
+
+
         //create a menu item and set up its listeners, similar to buttons
         JMenuItem miHelp = new JMenuItem("Help me");
         miHelp.addActionListener(this);
         miHelp.setActionCommand("help");
-    
+    	
+	JMenuItem miFile = new JMenuItem("Quit");
+	miFile.addActionListener(this);
+	miFile.setActionCommand("file");
+	
         //put together the pieces
         menuHelp.add(miHelp);
         menuBar.add(menuHelp);
+
+	menuFile.add(miFile);
+	menuBar.add(menuFile);
 
         //add bar to this JFrame
         setJMenuBar(menuBar);    
@@ -108,6 +120,9 @@ class NameGUI extends JFrame implements ActionListener{
             case "sample":
                 JOptionPane.showMessageDialog(this,"A Sample message dialog box","A plain message",JOptionPane.PLAIN_MESSAGE);
                 break;
+	    case "name":
+		JOptionPane.showMessageDialog(this, "Name", "Bradlee Cruise",JOptionPane.PLAIN_MESSAGE);
+		break;
             case "quit" :
                 System.exit(0);
                 break;
@@ -120,6 +135,9 @@ class NameGUI extends JFrame implements ActionListener{
             case "help" :
                 JOptionPane.showMessageDialog(this,"There is no help for you.","Sorry",JOptionPane.WARNING_MESSAGE);
                 break;
+	    case "file" :
+		JOptionPane.showMessageDialog(this, "Quit Program", "Quit"  , JOptionPane.WARNING_MESSAGE);
+		break;
                 
         }
     }
