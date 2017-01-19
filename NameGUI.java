2311405031e24d24d22b55272b9386b3ade62be9
Chelsie Danielle Collins <chcollins@ubuntu.statler.wvu.edu>
2017-01-19 10:03:28 -0500

    Lab 2 complete

--- NameGUI.java
+++ NameGUI.java
@@ -20,6 +20,7 @@ class NameGUI extends JFrame implements ActionListener{
         JButton btnSample = new JButton("Sample Button");
         JButton btnQuit = new JButton("Quit");
         JButton btnSwap = new JButton("Swap");
+	JButton btnName = new JButton("My Name"); 
         
         //create sample textboxes
         txtWord1 = new JTextField(15);
@@ -35,10 +36,13 @@ class NameGUI extends JFrame implements ActionListener{
         btnQuit.addActionListener(this);
         btnSwap.setActionCommand("swap");  
         btnSwap.addActionListener(this);
+	btnName.setActionCommand("name");
+	btnName.addActionListener(this);
  
         //Add components to proper panels
         panelTop.add(btnSample);
         panelTop.add(btnQuit);
+	panelTop.add(btnName);
         
         //panelMiddle.add(btnYourButton);
 
@@ -88,15 +92,21 @@ class NameGUI extends JFrame implements ActionListener{
 
         //create a menu  (file, edit, help, etc)
         JMenu menuHelp = new JMenu("Help");
+	JMenu menuFile = new JMenu("File"); 
         
         //create a menu item and set up its listeners, similar to buttons
         JMenuItem miHelp = new JMenuItem("Help me");
         miHelp.addActionListener(this);
         miHelp.setActionCommand("help");
+	JMenuItem miFile = new JMenuItem("Quit");
+	miFile.addActionListener(this);
+	miFile.setActionCommand("file"); 
     
         //put together the pieces
         menuHelp.add(miHelp);
         menuBar.add(menuHelp);
+	menuFile.add(miFile);
+	menuBar.add(menuFile);
 
         //add bar to this JFrame
         setJMenuBar(menuBar);    
@@ -117,9 +127,15 @@ class NameGUI extends JFrame implements ActionListener{
                 txtWord1.setText(txtWord2.getText());
                 txtWord2.setText(tempString);
                 break;
+	    case "name" :
+		JOptionPane.showMessageDialog(this, "Chelsie Collins", "Dialog Box for my name", JOptionPane.PLAIN_MESSAGE);
+		break;
             case "help" :
                 JOptionPane.showMessageDialog(this,"There is no help for you.","Sorry",JOptionPane.WARNING_MESSAGE);
                 break;
+	    case "file" :
+		JOptionPane.showMessageDialog(this, "Quitting", "File Quit", JOptionPane.WARNING_MESSAGE);
+		break; 
                 
         }
     }
