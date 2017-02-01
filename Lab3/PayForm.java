d47d180fe765a496e43cc92e5418b7655d27453f
Zed42 <cpd0001@mix.wvu.edu>
2017-01-31 19:26:34 -0500

    Fixing Dunn branch for the last time

--- Lab3/PayForm.java
+++ Lab3/PayForm.java
@@ -5,69 +5,72 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-class Pizza extends JFrame implements ActionListener{
-    
-    JTextField txtName;
-    JTextField txtAddress;
-    JTextField txtPhone;
+class PayForm extends JFrame implements ActionListener{
+    JTextField name;
+    JTextField cardNumber;
+    JTextField email;
     JTextArea txtOrder;
-    
     ButtonGroup btnGroupRadio;
     ArrayList<JCheckBox> btnCheck;
     
-    Pizza(){
+    PayForm(){
         JPanel panelMain = new JPanel();
         GroupLayout layout = new GroupLayout(panelMain);
         JPanel pnlButtons = new JPanel();
         JPanel pnlRadios = new JPanel();
         
-        txtName = new JTextField(20);
-        txtAddress = new JTextField(50);
-        txtPhone = new JTextField(10);
+        name = new JTextField(20);
+        cardNumber = new JTextField(50);
+        email = new JTextField(10);
         txtOrder = new JTextArea(15,50);
         txtOrder.setEditable(false);
         
-        JLabel lName = new JLabel("Name");
-        JLabel lAddress = new JLabel("Address");
-        JLabel lPhone = new JLabel("Phone");
-        JRadioButton rad12 = new JRadioButton("12 inch");
-        rad12.setActionCommand("12inch");
-        JRadioButton rad10 = new JRadioButton("10 inch");
-        rad10.setActionCommand("10inch");
-        rad10.setSelected(true);
-        JRadioButton rad16 = new JRadioButton("16 inch");
-        rad16.setActionCommand("16inch");
-        
+		ImageIcon amex = new ImageIcon("amex.jpg");
+		ImageIcon visa = new ImageIcon("visa.png");
+		ImageIcon master = new ImageIcon("master.png");
+		ImageIcon discover = new ImageIcon("discover.png");
+		
+        JLabel lName = new JLabel("Name:");
+        JLabel lCardNumber = new JLabel("Credit Card:");
+        JLabel lEmail = new JLabel("eMail:");
+        JRadioButton rad12 = new JRadioButton(amex);
+        rad12.setActionCommand("Amex");
+        JRadioButton rad10 = new JRadioButton(visa);
+        rad10.setActionCommand("Visa");
+        JRadioButton rad16 = new JRadioButton(master);
+        rad16.setActionCommand("Mastercard");
+		JRadioButton rad18 = new JRadioButton(discover);
+        rad18.setActionCommand("Discover");
+		rad10.setSelected(true);
+		
         btnGroupRadio = new ButtonGroup();
         btnGroupRadio.add(rad12);
         btnGroupRadio.add(rad10);
         btnGroupRadio.add(rad16);
+		btnGroupRadio.add(rad18);
 
-        JLabel lblSize = new JLabel("Size:  ");
+        JLabel lblSize = new JLabel("Card: ");
         lblSize.setFont(new Font(lblSize.getFont().getName(), Font.PLAIN, 20));
         pnlRadios.add(lblSize);
         pnlRadios.add(rad10);
         pnlRadios.add(rad12);
         pnlRadios.add(rad16);
+		pnlRadios.add(rad18);
         
-        JLabel lblToppings = new JLabel("Toppings:  ");
-        lblToppings.setFont(new Font(lblToppings.getFont().getName(), Font.PLAIN, 20));
-        JCheckBox chkPepperoni = new JCheckBox("Pepperoni");
-        chkPepperoni.setActionCommand("Pepperoni");
-        JCheckBox chkPeppers = new JCheckBox("Peppers");
-        chkPeppers.setActionCommand("Peppers");
-        JCheckBox chkSausage = new JCheckBox("Sausage");
-        chkSausage.setActionCommand("Sausage");
+        JLabel lblreceiptOpt = new JLabel("Receipt Options:");
+        lblreceiptOpt.setFont(new Font(lblreceiptOpt.getFont().getName(), Font.PLAIN, 20));
+        JCheckBox chkPrint = new JCheckBox("Printed Receipt");
+        chkPrint.setActionCommand("Printed Receipt");
+        JCheckBox chkEmail = new JCheckBox("eMail Confirmation");
+        chkEmail.setActionCommand("eMail");
         
         btnCheck = new ArrayList<JCheckBox>();
-        btnCheck.add(chkPepperoni);
-        btnCheck.add(chkPeppers);
-        btnCheck.add(chkSausage);
+        btnCheck.add(chkPrint);
+        btnCheck.add(chkEmail);
         
-        pnlButtons.add(lblToppings);
-        pnlButtons.add(chkPepperoni);
-        pnlButtons.add(chkPeppers);
-        pnlButtons.add(chkSausage);
+        pnlButtons.add(lblreceiptOpt);
+        pnlButtons.add(chkPrint);
+        pnlButtons.add(chkEmail);
         
         JButton btnOrder = new JButton("Place Order");
         btnOrder.setActionCommand("order");
@@ -81,11 +84,11 @@ class Pizza extends JFrame implements ActionListener{
            layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
                     .addComponent(lName)
-                    .addComponent(txtName)
-                    .addComponent(lAddress)
-                    .addComponent(txtAddress)
-                    .addComponent(lPhone)
-                    .addComponent(txtPhone)
+                    .addComponent(name)
+                    .addComponent(lCardNumber)
+                    .addComponent(cardNumber)
+                    .addComponent(lEmail)
+                    .addComponent(email)
                     .addComponent(pnlRadios)
                     .addComponent(pnlButtons)
                     .addComponent(btnOrder))
@@ -95,11 +98,11 @@ class Pizza extends JFrame implements ActionListener{
             layout.createParallelGroup()
                 .addGroup(layout.createSequentialGroup()
                     .addComponent(lName)
-                    .addComponent(txtName)
-                    .addComponent(lAddress)
-                    .addComponent(txtAddress)
-                    .addComponent(lPhone)
-                    .addComponent(txtPhone)
+                    .addComponent(name)
+                    .addComponent(lCardNumber)
+                    .addComponent(cardNumber)
+                    .addComponent(lEmail)
+                    .addComponent(email)
                     .addComponent(pnlRadios)
                     .addComponent(pnlButtons)
                     .addComponent(btnOrder))
@@ -110,20 +113,12 @@ class Pizza extends JFrame implements ActionListener{
 
         //tells java what to do when the class object closes
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Pizza Order");
-
-        
-        //get visible container and add panelMain to it
-        //EVERYTHING has to be arranged and set before adding to ContentPane
+        setTitle("Pay Form");
         getContentPane().add(panelMain);
-
-        //this centers the window in the screen
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
         int x = (int) ((dimension.getWidth() - this.getWidth()) / 2);
         int y = (int) ((dimension.getHeight() - this.getHeight()) / 2);
         setLocation(x, y);
-        
-        //make sure you can actually see it, starts off false
         setVisible(true);       
 
         
@@ -132,17 +127,19 @@ class Pizza extends JFrame implements ActionListener{
     
     public void actionPerformed(ActionEvent e){
            if(e.getActionCommand().equals("order")){
+			   if(name.getText().equals("") || cardNumber.getText().equals("") || email.getText().equals(""))
+				   JOptionPane.showMessageDialog(this,"All fields must be filled, or we cannot complete your order. Please try again.","Error",JOptionPane.WARNING_MESSAGE);
                StringBuffer strOrder = new StringBuffer();
-               strOrder.append(txtName.getText() + "\n");
-               strOrder.append(txtAddress.getText() + "\n");
-               strOrder.append(txtPhone.getText() + "\n");
+               strOrder.append(name.getText() + "\n");
+               strOrder.append(cardNumber.getText() + "\n");
+               strOrder.append(email.getText() + "\n");
                strOrder.append(btnGroupRadio.getSelection().getActionCommand() + "\n");
                
                Iterator<JCheckBox> iterCheckButtons = btnCheck.iterator();
                while(iterCheckButtons.hasNext()){
                    JCheckBox nextBox = iterCheckButtons.next();
                    if(nextBox.isSelected()){
-                       strOrder.append(nextBox.getActionCommand() + " ");
+                       strOrder.append(nextBox.getActionCommand() + ", ");
                        
                    }
                    
@@ -166,6 +163,6 @@ class Pizza extends JFrame implements ActionListener{
     
     
     public static void main(String args[]){
-        new Pizza();
+        new PayForm();
     }
 }
