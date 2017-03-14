7f693e59ca51d0d9f7eb9a6278c826da608c3b15
Sarah Lynn Moore <slmoore1@ubuntu.statler.wvu.edu>
2017-03-14 19:35:45 -0400

    GUI is done

--- Server_Client/Assignment6/Client.java
+++ Server_Client/Assignment6/Client.java
@@ -1,12 +1,16 @@
 import javax.swing.*;
 import java.awt.event.*;
-import java.awt.ToolKit;
+import java.awt.Toolkit;
 import java.awt.Dimension;
 import java.net.*;
-import java.io.*
+import java.io.*;
 
 class Client extends JFrame implements ActionListener{
-	JTextField txtField
+	JTextField txtField;
+	JTextArea txtArea;
+	Socket sock;
+	PrintWriter pwOut;
+	BufferedReader brIn;
 
 	Client(){
 		JPanel pnlMain = new JPanel();
@@ -14,10 +18,11 @@ class Client extends JFrame implements ActionListener{
 
 		JPanel pnlButtons = new JPanel();
 		JPanel pnlTxtField = new JPanel();
+		JPanel pnlOutput = new JPanel();
 		
 		 
-		txtField = new JTextField(200);
-		JLabel lblTxtField = new JLabel("Comma Seperated List of Names");
+		txtField = new JTextField(15);
+		JLabel lblTxtField = new JLabel("Comma Seperated List of Names: ");
 		pnlTxtField.add(lblTxtField);
 		pnlTxtField.add(txtField);
 		
@@ -30,9 +35,56 @@ class Client extends JFrame implements ActionListener{
         	btnQuit.addActionListener(this);
 
        		pnlButtons.add(btnSend);
-        	pnlButtons.add(btn);
+        	pnlButtons.add(btnQuit);
+
+		txtArea = new JTextArea(50,100);
+		txtArea.setEditable(false);
+		JLabel lblReturn = new JLabel("Output from Server");
+		pnlOutput.add(lblReturn);
+		pnlOutput.add(txtArea);
+
+		        
+        	pnlMain.add(pnlTxtField);
+        	pnlMain.add(pnlButtons);
+        	pnlMain.add(pnlOutput);
+		
+        	getContentPane().add(pnlMain);
         
-        	
+        	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        	setTitle("Connect to Server Exercise");        
+        	setSize(700,200);		
+		
+        	Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        	int x = (int) ((dimension.getWidth() - this.getWidth()) / 2);
+        	int y = (int) ((dimension.getHeight() - this.getHeight()) / 2);
+        	setLocation(x, y);
+        
+        	setVisible(true);
+        	//run();	
 	}
+
+	//private void run(){
+	//}
+
+	public void actionPerformed(ActionEvent e){
+		if (!sock.isClosed()){
+		    switch (e.getActionCommand()){
+		        case "send":
+		           //Fill in 
+		        break;
+		        case "quit":
+		            pwOut.println("<die>");
+		            System.exit(0);
+		        break;
+		    }
+		}else{
+		    JOptionPane.showMessageDialog(this,"Socket is Closed","Error",JOptionPane.ERROR_MESSAGE);
+		}   
+   	} 
+
+	public static void main(String args[]){
+		new Client();	
+	}	
+
 	
 }
